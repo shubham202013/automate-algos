a9eb196d100f5ae3250c38116afe1a93a7d0e1d3
shubham202013 <shubhamkanzariya2013@gmail.com>
2024-09-09 20:38:37 +0530

    changes

---
+++ js/submittion.js
@@ -0,0 +1,21 @@
+document.getElementById('registration-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const formData = new FormData(this);
+
+    fetch('https://submit-form.com/HDy2yNVOU', {
+            method: 'POST',
+            // body: formData,
+        })
+        .then(response => {
+            if (response.ok) {
+                window.location.href = 'https://automatealgos-landing-page.vercel.app/html/thankyou-page.html';
+            } else {
+                alert('There was an error with your submission.');
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('There was an error submitting the form.');
+        });
+});
